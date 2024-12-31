f62e6f4f63687374bf9df1d35194de187f4e882d
RaphaelBajon <raphael.bajon@protonmail.com>
2024-12-31 12:29:46 +0100

    version

--- canyonbpy/__init__.py
+++ canyonbpy/__init__.py
@@ -1,5 +1,4 @@
 from .core import canyonb
 from .utils import calculate_decimal_year, adjust_arctic_latitude
 
-__version__ = "0.1.0"
 __all__ = ["canyonb", "calculate_decimal_year", "adjust_arctic_latitude"]
