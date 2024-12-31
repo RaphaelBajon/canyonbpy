c2cc3f0a81cbdb420ec21fe6191e8c62feb402da
Raphaël Bajon <94925217+RaphaelBajon@users.noreply.github.com>
2024-12-31 13:04:29 +0100

    Update __init__.py

--- canyonbpy/__init__.py
+++ canyonbpy/__init__.py
@@ -1,4 +1,5 @@
 from .core import canyonb
 from .utils import calculate_decimal_year, adjust_arctic_latitude
 
+__version__ = "0.0.0"
 __all__ = ["canyonb", "calculate_decimal_year", "adjust_arctic_latitude"]
